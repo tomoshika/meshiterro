5ddc56d6317bd29369a14863d2c3b947716a3099
tomoshika <tmm.orange.skm@gmail.com>
2021-08-10 13:20:26 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'e56087619c36091de04f342d49011f49004cc1025a61ea07576bde9e020ab22aeea81e890c3036a1e7e8b357aefb343432e9f70e79f8f1ea52f42ee870802374'

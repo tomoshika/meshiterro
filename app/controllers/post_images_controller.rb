6fe0057a09c3dfeb33ead5b7dec5b12ae14fe480
tomoshika <tmm.orange.skm@gmail.com>
2021-08-12 09:35:04 +0000

    [Finish]4-5

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,7 +1,7 @@
 class PostImagesController < ApplicationController
   
   def new
-     @post_image = PostImage.new
+    @post_image = PostImage.new
   end
 
   def create
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,21 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @post_images = @user.post_images.page(params[:page]).reverse_order
   end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def updadte
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+  
+  
+  
+  
   
 end
